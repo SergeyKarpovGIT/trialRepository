a22f29f779d1c2865a83c1315a3271edda5b2174
Sergey Karpov <163626106+SergeyKarpovGIT@users.noreply.github.com>
2024-03-17 12:17:25 +0300

    Update torque.f95

--- torque.f95
+++ torque.f95
@@ -1,4 +1,17 @@
 program main
-! implicit none is implemented during compilation '-Wall -fimplicit-none -std=f2018...'
+! The program converts Nm to Kg applied to a shaft of any length
+implicit none
+use, intrinsic :: iso_fortran_env, only: qp => real128
 
+  real(qp), parameter :: g = 9.80665_qp
+  real(qp) :: Length, r, Force, tau
+
+  print '(a)', 'Enter L in cm:'
+  read *, Length
+  print '(a)', 'Enter desired Nm:'
+  read *, Force
+
+  r = Length * 0.01_qp
+  tau = Force / r
+  print '(a,f6.3,a)', 'Apply ', tau / g, ' kg'
 end program main
